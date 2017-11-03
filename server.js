41850e3d59907f0c5822af1faaeb5856702497f0
xiandairen <8198125202qq.com>
2017-11-03 18:26:19 +0800

    Rejected commit: Default

--- server.js
+++ server.js
@@ -2,7 +2,12 @@
  * Created by Administrator on 2017/10/26.
  */
 const http = require("http");
+const test = require("./test");
 http.createServer((res, req) => {
-    req.writeHead(200, {"Content-Type": "text/html"});
-    req.end("<div>node server run port 3000</div>")
+    req.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
+    let list = "";
+    for (let j  in req){
+        list += `<div>${j}${test()} </div>`
+    }
+    req.end(`${list} <hr/>${req.getHeader('Connection')}`)
 }).listen(3000);
---
+++ test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/10/26.
+ */
+
+
+
+function test(){
+    return "___";
+}
+
+export test();
